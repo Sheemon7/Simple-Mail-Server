cd096b984cc7a60be82783a0aae71ffb1db0e0fe
Simon Mandlik <simon.mandlik@gmail.com>
2017-01-02 18:41:24 +0100

    server nearly finished

--- server/server.c
+++ server/server.c
@@ -15,7 +15,7 @@
 #define MAX_PORT_LENGTH 10
 
 int bind_server_to_port(struct addrinfo *servinfo);
-
+void serve_clients(int socket_fd);
 int main(int argc, char *argv[]) {
 	char server_port[MAX_PORT_LENGTH];
 	struct addrinfo *servinfo, aux;
@@ -42,14 +42,51 @@ int main(int argc, char *argv[]) {
 	freeaddrinfo(servinfo);
 
 	// listen
+	if (listen(socket_fd, BACKLOG) == -1) {
+		perror("Failed to listen");
+		exit(1);
+	}
+	
+	// TODO reap dead processes
 
 	// serve incoming clients
+	printf("Ready and waiting for connections...\n");
+	
+	
+	
 	return 0;
+}
+
+void serve_clients(int socket_fd) {
+	int connect_fd;
+	struct sockaddr_storage client_addr;
+	socklen_t sin_size;
+	struct sigaction sa;
+	
+	while(1) {
+		sin_size = sizeof(client_addr);
+		// get new fd for new connection
+		connect_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &sin_size);
+		if (connect_fd == -1) {
+			perror("Failed to accept connection");
+			continue;
+		}
+		
+		// child process - 0 returned by fork
+		if (!fork()) {
+			close(socket_fd); // close the listener
+			// send data
+			
+			close(connect_fd);
+		}		
+		close(connect_fd); // close in parent
+	}
 }
 
 int bind_server_to_port(struct addrinfo *servinfo) {
 	struct addrinfo *p;
 	int socket_fd;
+	int yes = 1;
 	for(p = servinfo; p != NULL; p = p->ai_next) {
 		// try to create a socket
 		if ((socket_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
@@ -57,7 +94,10 @@ int bind_server_to_port(struct addrinfo *servinfo) {
 			continue;
 		}	
 
-		// recover free ports
+		// allow the programme to reuse the port if there is something still hanging
+		if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+			perror("Error setting socket options");
+			exit(1);
 
 		// bind socket to its port
 		if (bind(socket_fd, p->ai_addr, p->ai_addrlen) == -1) {
@@ -65,6 +105,9 @@ int bind_server_to_port(struct addrinfo *servinfo) {
 			perror("Failed to bind socket to port");
 			continue;
 		}
+		return socket_fd;
 	}
+	fprintf(stderr, "Failed to bind to any port");
+	exit(1);	
 }
 
